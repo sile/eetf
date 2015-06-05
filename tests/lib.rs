716ed609afa4808f862af39f901535a0e274bb43
Takeru Ohta <phjgt308@gmail.com>
2015-06-05 10:04:55 +0900

    Define assert_decode!: an auxiliary macro for unit-tests

--- tests/lib.rs
+++ tests/lib.rs
@@ -4,29 +4,35 @@ extern crate eetf;
 
 use eetf::*;
 
+macro_rules! assert_decode {
+    ($x:expr, $y:expr) => {
+        assert_eq!($x, decode(&$y))
+    }
+}
+
 #[test]
 fn decode_empty() {
     let input = [];
     let expected = None;
-    assert_eq!(expected, decode(&input));
+    assert_decode!(expected, input);
 }
 
 #[test]
 fn decode_small_integer() {
     let input = [131,97,5];
     let expected = 5;
-    assert_eq!(Term::Int(expected), decode(&input).unwrap());
+    assert_decode!(Some(Term::Int(expected)), input);
 }
 
 #[test]
 fn decode_integer() {
     let input = [131,98,0,0,4,210];
     let expected = 1234;
-    assert_eq!(Term::Int(expected), decode(&input).unwrap());
+    assert_decode!(Some(Term::Int(expected)), input);
 
     let input = [131,98,255,255,251,46];
     let expected = -1234;
-    assert_eq!(Term::Int(expected), decode(&input).unwrap());
+    assert_decode!(Some(Term::Int(expected)), input);
 }
 
 // #[test]
@@ -47,12 +53,12 @@ fn decode_integer() {
 fn decode_atom() {
     let input = [131,100,0,4,104,111,103,101];
     let expected = "hoge".to_string();
-    assert_eq!(Term::Atom(expected), decode(&input).unwrap());
+    assert_decode!(Some(Term::Atom(expected)), input);
 }
 
 #[test]
 fn decode_nil() {
     let input = [131,106];
     let expected = vec![];
-    assert_eq!(Term::List(expected), decode(&input).unwrap());
+    assert_decode!(Some(Term::List(expected)), input);
 }
