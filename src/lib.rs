3c5c99b3b62c691181e11b869da1c426dadc5406
Takeru Ohta <phjgt308@gmail.com>
2022-03-23 21:44:49 +0900

    Fix clippy warnings

--- src/lib.rs
+++ src/lib.rs
@@ -186,7 +186,7 @@ impl fmt::Display for Atom {
         write!(
             f,
             "'{}'",
-            self.name.replace("\\", "\\\\").replace("'", "\\'")
+            self.name.replace('\\', "\\\\").replace('\'', "\\'")
         )
     }
 }
